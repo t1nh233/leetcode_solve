95bb3f6dad106423a04718360d2b9130bf4ef229
t1nh233 <whaletrieuquoctinh@gmail.com>
2025-09-14 14:32:22 +0700

    Update Code.py

--- google_1year/1.Two Sum/Code.py
+++ google_1year/1.Two Sum/Code.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
+        for i, num in enumerate(nums):
+            need = target - num
+            if need in num_map:
+                return [num_map[need], i]
+            num_map[num] = i
+        return []
